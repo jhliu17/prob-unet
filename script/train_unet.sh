be763d878dd59ad70a7f7d77e2ddb5effb5cff21
jhliu17 <junhaoliu17@gmail.com>
2023-05-25 11:21:57 -0700

    update dataset

--- script/train_unet.sh
+++ script/train_unet.sh
@@ -2,12 +2,12 @@ export CUDA_VISIBLE_DEVICES=$1
 
 python run.py --mode train \
               --git_info `git describe --always`\
-              --seed 12 \
+              --seed 2023 \
               --train_dataset_path /srv/disk00/junhal11/oct_understanding/data/2015_boe_chiu/2015_BOE_Chiu/train_set_wo_xobject.npz \
               --test_dataset_path /srv/disk00/junhal11/oct_understanding/data/2015_boe_chiu/2015_BOE_Chiu/test_set_wo_xobject.npz  \
               --batch_size 8 \
               --lr 1e-2 \
-              --epoch 100 \
+              --epoch 200 \
               --number_worker 2 \
               --log_freq 10 \
               --eval_freq 20 \
