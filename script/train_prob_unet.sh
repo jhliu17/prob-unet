c61a363876bfffd929189e625d6be4c878113e43
jhliu17 <junhaoliu17@gmail.com>
2023-05-27 14:58:03 -0700

    use `latent_size=8` as default

--- script/train_prob_unet.sh
+++ script/train_prob_unet.sh
@@ -7,7 +7,7 @@ python run.py --mode train \
               --test_dataset_path /srv/disk00/junhal11/oct_understanding/data/2015_boe_chiu/2015_BOE_Chiu/test_set_wo_xobject.npz  \
               --batch_size 8 \
               --lr 1e-2 \
-              --latent_size 4 \
+              --latent_size 8 \
               --epoch 300 \
               --number_worker 2 \
               --log_freq 10 \
